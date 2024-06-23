7b91b31f607987d376ddc1fffd00d2cf676906f2
salman3009 <salman444g@gmail.com>
2024-06-23 10:46:02 +0530

    Code

--- first-aicte-app/src/App.js
+++ first-aicte-app/src/App.js
@@ -1,9 +1,56 @@
 import './App.css';
 import Product from './Product';
 const App = ()=>{
+
+    const list=[
+        {
+            name:"mi1",
+            price:2000,
+            image:'./assets/mi1.png'
+        },{
+            name:"mi2",
+            price:4000,
+            image:'./assets/mi2.png' 
+        },
+        {
+            name:"mi1",
+            price:2000,
+            image:'./assets/mi1.png'
+        },{
+            name:"mi2",
+            price:4000,
+            image:'./assets/mi2.png' 
+        },
+        {
+            name:"mi1",
+            price:2000,
+            image:'./assets/mi1.png'
+        },{
+            name:"mi2",
+            price:4000,
+            image:'./assets/mi2.png' 
+        },
+        {
+            name:"mi1",
+            price:2000,
+            image:'./assets/mi1.png'
+        },{
+            name:"mi2",
+            price:4000,
+            image:'./assets/mi2.png' 
+        }
+    ]
      
     return (<div style={{display:"flex",flexWrap:'wrap'}}>
-        <Product name="mi\1" price="8000" image="./assets/mi1.png"/>
+            {list.map((obj)=>{
+                 return(<div>
+                    <Product
+                     name={obj.name}
+                     price={obj.price}
+                     image={obj.image}
+                    />
+                 </div>)
+            })}
     </div>)
 }
 
