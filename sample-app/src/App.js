57f35dc8a1026e9a8e46e24b7d1d5dd9ab4a7310
salman3009 <salman444g@gmail.com>
2024-06-25 09:51:00 +0530

    code

--- sample-app/src/App.js
+++ sample-app/src/App.js
@@ -10,13 +10,25 @@ function App() {
          setData(getData+1);
   }
 
+  const subtraction=(n,type)=>{
+      if(type=="mul"){
+        setData(getData*n);
+      }
+      if(type == 'sub'){
+        setData(getData-n);
+      }
+  }
+
  
 
   return (
    <div>
-      <h1 style={{textAlign:'center'}}>state-{getData}</h1>
+      <h1 style={{textAlign:'center'}}>state={getData}</h1>
       <button onClick={addition}>Add</button> 
+      <br/>
+      <button onClick={()=>subtraction(6,'mul')}>Sub</button>
    </div>
+
   );
 }
 
