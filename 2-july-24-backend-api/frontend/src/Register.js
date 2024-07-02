e30bcd90c7bf0b91f3a1b9d3457d46273e23d666
salman3009 <salman444g@gmail.com>
2024-07-02 09:04:49 +0530

    code

--- 2-july-24-backend-api/frontend/src/Register.js
+++ 2-july-24-backend-api/frontend/src/Register.js
@@ -1,9 +1,28 @@
-
+import { useState,useEffect } from "react";
 
 function Register(){
 
+    const [getData,setData] = useState(0);
+
+
+     useEffect(()=>{
+           alert("effect initial");
+     },[])
+
+    function initial(){
+         alert("initial");
+    }
+
+    initial();
+
+
+    function increment(){
+        setData(getData+1);
+    }
+
     return (<div>
         <h1>Register component</h1>
+        <button onClick={increment}>Increment</button>
     </div>)
 }
 
